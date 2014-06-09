261ae0d261c0ddd669ec7f1d01222f618b1a382d
guanhao97 <guanhao3797@gmail.com>
2014-06-09 21:53:27 +0800

    Added CommandError and interactive terminal

--- shelldon.py
+++ shelldon.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python
 
 import os
+import sys
 import shlex
 import subprocess
 
 home = os.path.expanduser('~')
 os.chdir(home)
+
+class CommandError(Exception):
+    def __init__(self, message):
+        # Call the base class constructor with the parameters it needs
+        Exception.__init__(self, message)
 
 def call(cmd):
     cmd_lines = split_cmd(cmd)
@@ -16,7 +22,10 @@ def call(cmd):
         
         except OSError:
             if call_list[0] == 'cd':
-                os.chdir(call_list[1])
+                try: 
+                    os.chdir(call_list[1])
+                except:
+                    raise CommandError('cd requires a directory!')
 
 def split_cmd(cmd):
     cmd_lines = cmd.splitlines()
@@ -27,4 +36,22 @@ def split_cmd(cmd):
 
     cmd_lines = filter(lambda x: x != '', cmd_lines)
     return cmd_lines
+
+def terminal():
+    print ("Shelldon's interactive terminal.\n" + 
+           "Enter 'quit' to exit and 'help' for documentation.\n")
+
+    while True:
+        cwd = os.getcwd()
+        cwd = cwd.replace(home, '~')
+        cmd = raw_input(cwd + '$ ')
+        
+        if cmd == 'quit':
+            break
+
+        elif cmd == 'help':
+            print """This is the incomplete help page"""
+
+        else:
+            call(cmd)
 
