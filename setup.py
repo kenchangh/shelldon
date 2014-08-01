9338d5f84b975b6336ea7fae9f0a3100118d0faa
Maverick Chan <guanhao3797@gmail.com>
2014-08-01 09:08:08 +0800

    More details in setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@ setup(
     version = '0.0.1',
     packages = ['shelldon'],
     license = 'MIT',
+    author = 'Maverick Chan',
+    author_email = 'guanhao3797@gmail.com'
     long_description = open('README.md').read(),
     description = 'Python-wrapper of Bash',
     url = 'https://github.com/maverick97/shelldon',
