f65c3918b1a8bf7ca809b5dab5acb52c82f2af5b
Maverick Chan <guanhao3797@gmail.com>
2014-08-02 19:12:42 +0800

    Tested and fixed bugs

--- shelldon/shelldon.py
+++ shelldon/shelldon.py
@@ -20,11 +20,14 @@ def call(cmd):
         except OSError:
             if call_list[0] == 'cd':
                 try: 
+                    # ~ will raise CommandError, need to expand
+                    if call_list[1][0] == '~':
+                        call_list[1] = call_list[1].replace('~',
+                                       os.path.expanduser('~')) 
                     os.chdir(call_list[1])
                 except:
-                    raise CommandError(
-                    '{cmd} is not a valid command!'.format(
-                    cmd = cmd))
+                    raise CommandError('{0} is not a valid command.'.format(
+                                       call_list[0]))
 
 def split_cmd(cmd):
     cmd_lines = cmd.splitlines()
@@ -38,26 +41,30 @@ def terminal():
 
     print ("\nShelldon's interactive terminal.\n" + 
            "Enter 'quit' to exit and 'help' for documentation.")
+    
+    try:
+        while True:
+            cwd = os.getcwd()
+            cwd = cwd.replace(home, '~')
+            cmd = raw_input('\n' + cwd + '$ ')
 
-    while True:
-        cwd = os.getcwd()
-        cwd = cwd.replace(home, '~')
-        cmd = raw_input('\n' + cwd + '$ ')
-
-        if cmd == 'quit':
-            break
-
-        elif cmd == 'help':
-            help_dir = os.path.join(os.path.dirname(__file__), 'help', 'help.txt')
-            with open(help_dir, 'r') as f:
-                help_page = f.read()
-            print help_page
-
-        else:
-            try:
-                call(cmd)
-            except EOFError:
+            if cmd == 'quit':
                 break
-            if not cmd:
-                break
+
+            elif cmd == 'help':
+                help_dir = os.path.join(os.path.dirname(__file__),
+                                        'help', 'help.txt')
+                with open(help_dir, 'r') as f:
+                    help_page = f.read()
+                print help_page
+
+            else:
+                if cmd:
+                    call(cmd)
+                if not cmd:
+                    break
+    except EOFError:
+        # Quit on ^D
+        print '\n'
+        
 
